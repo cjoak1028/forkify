abb377a33fb2472cc4c370de9a025f94bc2fc9b4
David Kim <cjoak1028@gmail.com>
2022-02-02 15:43:17 -0500

    pagination implementation - 1

--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,14 @@
-import { API_URL } from './config.js';
+import { async } from 'regenerator-runtime';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
     recipe: {},
     search: {
         query: '',
-        results: []
+        results: [],
+        page: 1,
+        resultsPerPage: RES_PER_PAGE
     }
 }
 
@@ -51,3 +54,12 @@ export const loadSearchResults = async function (query) {
         throw err;
     }
 };
+
+export const getSearchResultsPage = function (page = state.search.page) {
+    state.search.page = page;
+
+    const start = (page - 1) * state.search.resultsPerPage; // 0
+    const end = page * state.search.resultsPerPage; // 9
+
+    return state.search.results.slice(start, end);
+}
